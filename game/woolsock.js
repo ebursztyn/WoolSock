edc82f5bf59a3ab431f72bee993993583f572c57
Erez Bursztyn <erez@slatescience.com>
2016-05-15 20:32:43 +0300

    work on end animation

--- game/woolsock.js
+++ game/woolsock.js
@@ -1,3 +1,471 @@
+var SONG_FRAMES = [
+    "tetaaru00025.png",
+    "tetaaru00037.png",
+    "tetaaru00049.png",
+    "tetaaru00061.png",
+    "tetaaru00073.png",
+    "tetaaru00085.png",
+    "tetaaru00097.png",
+    "tetaaru00109.png",
+    "tetaaru00121.png",
+    "tetaaru00133.png",
+    "tetaaru00145.png",
+    "tetaaru00157.png",
+    "tetaaru00169.png",
+    "tetaaru00181.png",
+    "tetaaru00193.png",
+    "tetaaru00205.png",
+    "tetaaru00217.png",
+    "tetaaru00229.png",
+    "tetaaru00241.png",
+    "tetaaru00253.png",
+    "tetaaru00265.png",
+    "tetaaru00277.png",
+    "tetaaru00289.png",
+    "tetaaru00301.png",
+    "tetaaru00313.png",
+    "tetaaru00325.png",
+    "tetaaru00337.png",
+    "tetaaru00349.png",
+    "tetaaru00361.png",
+    "tetaaru00373.png",
+    "tetaaru00385.png",
+    "tetaaru00397.png",
+    "tetaaru00409.png",
+    "tetaaru00421.png",
+    "tetaaru00433.png",
+    "tetaaru00445.png",
+    "tetaaru00457.png",
+    "tetaaru00469.png",
+    "tetaaru00481.png",
+    "tetaaru00493.png",
+    "tetaaru00505.png",
+    "tetaaru00517.png",
+    "tetaaru00529.png",
+    "tetaaru00541.png",
+    "tetaaru00553.png",
+    "tetaaru00565.png",
+    "tetaaru00577.png",
+    "tetaaru00589.png",
+    "tetaaru00601.png",
+    "tetaaru00613.png",
+    "tetaaru00625.png",
+    "tetaaru00637.png",
+    "tetaaru00649.png",
+    "tetaaru00661.png",
+    "tetaaru00673.png",
+    "tetaaru00685.png",
+    "tetaaru00697.png",
+    "tetaaru00709.png",
+    "tetaaru00721.png",
+    "tetaaru00733.png",
+    "tetaaru00745.png",
+    "tetaaru00757.png",
+    "tetaaru00769.png",
+    "tetaaru00781.png",
+    "tetaaru00793.png",
+    "tetaaru00805.png",
+    "tetaaru00817.png",
+    "tetaaru00829.png",
+    "tetaaru00841.png",
+    "tetaaru00853.png",
+    "tetaaru00865.png",
+    "tetaaru00877.png",
+    "tetaaru00889.png",
+    "tetaaru00901.png",
+    "tetaaru00913.png",
+    "tetaaru00925.png",
+    "tetaaru00937.png",
+    "tetaaru00949.png",
+    "tetaaru00961.png",
+    "tetaaru00973.png",
+    "tetaaru00985.png",
+    "tetaaru00997.png",
+    "tetaaru01009.png",
+    "tetaaru01021.png",
+    "tetaaru01033.png",
+    "tetaaru01045.png",
+    "tetaaru01057.png",
+    "tetaaru01069.png",
+    "tetaaru01081.png",
+    "tetaaru01093.png",
+    "tetaaru01105.png",
+    "tetaaru01117.png",
+    "tetaaru01129.png",
+    "tetaaru01141.png",
+    "tetaaru01153.png",
+    "tetaaru01165.png",
+    "tetaaru01177.png",
+    "tetaaru01189.png",
+    "tetaaru01201.png",
+    "tetaaru01213.png",
+    "tetaaru01225.png",
+    "tetaaru01237.png",
+    "tetaaru01249.png",
+    "tetaaru01261.png",
+    "tetaaru01273.png",
+    "tetaaru01285.png",
+    "tetaaru01297.png",
+    "tetaaru01309.png",
+    "tetaaru01321.png",
+    "tetaaru01333.png",
+    "tetaaru01345.png",
+    "tetaaru01357.png",
+    "tetaaru01369.png",
+    "tetaaru01381.png",
+    "tetaaru01393.png",
+    "tetaaru01405.png",
+    "tetaaru01417.png",
+    "tetaaru01429.png",
+    "tetaaru01441.png",
+    "tetaaru01453.png",
+    "tetaaru01465.png",
+    "tetaaru01477.png",
+    "tetaaru01489.png",
+    "tetaaru01501.png",
+    "tetaaru01513.png",
+    "tetaaru01525.png",
+    "tetaaru01537.png",
+    "tetaaru01549.png",
+    "tetaaru01561.png",
+    "tetaaru01573.png",
+    "tetaaru01585.png",
+    "tetaaru01597.png",
+    "tetaaru01609.png",
+    "tetaaru01621.png",
+    "tetaaru01633.png",
+    "tetaaru01645.png",
+    "tetaaru01657.png",
+    "tetaaru01669.png",
+    "tetaaru01681.png",
+    "tetaaru01693.png",
+    "tetaaru01705.png",
+    "tetaaru01717.png",
+    "tetaaru01729.png",
+    "tetaaru01741.png",
+    "tetaaru01753.png",
+    "tetaaru01765.png",
+    "tetaaru01777.png",
+    "tetaaru01789.png",
+    "tetaaru01801.png",
+    "tetaaru01813.png",
+    "tetaaru01825.png",
+    "tetaaru01837.png",
+    "tetaaru01849.png",
+    "tetaaru01861.png",
+    "tetaaru01873.png",
+    "tetaaru01885.png",
+    "tetaaru01897.png",
+    "tetaaru01909.png",
+    "tetaaru01921.png",
+    "tetaaru01933.png",
+    "tetaaru01945.png",
+    "tetaaru01957.png",
+    "tetaaru01969.png",
+    "tetaaru01981.png",
+    "tetaaru01993.png",
+    "tetaaru02005.png",
+    "tetaaru02017.png",
+    "tetaaru02029.png",
+    "tetaaru02041.png",
+    "tetaaru02053.png",
+    "tetaaru02065.png",
+    "tetaaru02077.png",
+    "tetaaru02089.png",
+    "tetaaru02101.png",
+    "tetaaru02113.png",
+    "tetaaru02125.png",
+    "tetaaru02137.png",
+    "tetaaru02149.png",
+    "tetaaru02161.png",
+    "tetaaru02173.png",
+    "tetaaru02185.png",
+    "tetaaru02197.png",
+    "tetaaru02209.png",
+    "tetaaru02221.png",
+    "tetaaru02233.png",
+    "tetaaru02245.png",
+    "tetaaru02257.png",
+    "tetaaru02269.png",
+    "tetaaru02281.png",
+    "tetaaru02293.png",
+    "tetaaru02305.png",
+    "tetaaru02317.png",
+    "tetaaru02329.png",
+    "tetaaru02341.png",
+    "tetaaru02353.png",
+    "tetaaru02365.png",
+    "tetaaru02377.png",
+    "tetaaru02389.png",
+    "tetaaru02401.png",
+    "tetaaru02413.png",
+    "tetaaru02425.png",
+    "tetaaru02437.png",
+    "tetaaru02449.png",
+    "tetaaru02461.png",
+    "tetaaru02473.png",
+    "tetaaru02485.png",
+    "tetaaru02497.png",
+    "tetaaru02509.png",
+    "tetaaru02521.png",
+    "tetaaru02533.png",
+    "tetaaru02545.png",
+    "tetaaru02557.png",
+    "tetaaru02569.png",
+    "tetaaru02581.png",
+    "tetaaru02593.png",
+    "tetaaru02605.png",
+    "tetaaru02617.png",
+    "tetaaru02629.png",
+    "tetaaru02641.png",
+    "tetaaru02653.png",
+    "tetaaru02665.png",
+    "tetaaru02677.png",
+    "tetaaru02689.png",
+    "tetaaru02701.png",
+    "tetaaru02713.png",
+    "tetaaru02725.png",
+    "tetaaru02737.png",
+    "tetaaru02749.png",
+    "tetaaru02761.png",
+    "tetaaru02773.png",
+    "tetaaru02785.png",
+    "tetaaru02797.png",
+    "tetaaru02809.png",
+    "tetaaru02821.png",
+    "tetaaru02833.png",
+    "tetaaru02845.png",
+    "tetaaru02857.png",
+    "tetaaru02869.png",
+    "tetaaru02881.png",
+    "tetaaru02893.png",
+    "tetaaru02905.png",
+    "tetaaru02917.png",
+    "tetaaru02929.png",
+    "tetaaru02941.png",
+    "tetaaru02953.png",
+    "tetaaru02965.png",
+    "tetaaru02977.png",
+    "tetaaru02989.png",
+    "tetaaru03001.png",
+    "tetaaru03013.png",
+    "tetaaru03025.png",
+    "tetaaru03037.png",
+    "tetaaru03049.png",
+    "tetaaru03061.png",
+    "tetaaru03073.png",
+    "tetaaru03085.png",
+    "tetaaru03097.png",
+    "tetaaru03109.png",
+    "tetaaru03121.png",
+    "tetaaru03133.png",
+    "tetaaru03145.png",
+    "tetaaru03157.png",
+    "tetaaru03169.png",
+    "tetaaru03181.png",
+    "tetaaru03193.png",
+    "tetaaru03205.png",
+    "tetaaru03217.png",
+    "tetaaru03229.png",
+    "tetaaru03241.png",
+    "tetaaru03253.png",
+    "tetaaru03265.png",
+    "tetaaru03277.png",
+    "tetaaru03289.png",
+    "tetaaru03301.png",
+    "tetaaru03313.png",
+    "tetaaru03325.png",
+    "tetaaru03337.png",
+    "tetaaru03349.png",
+    "tetaaru03361.png",
+    "tetaaru03373.png",
+    "tetaaru03385.png",
+    "tetaaru03397.png",
+    "tetaaru03409.png",
+    "tetaaru03421.png",
+    "tetaaru03433.png",
+    "tetaaru03445.png",
+    "tetaaru03457.png",
+    "tetaaru03469.png",
+    "tetaaru03481.png",
+    "tetaaru03493.png",
+    "tetaaru03505.png",
+    "tetaaru03517.png",
+    "tetaaru03529.png",
+    "tetaaru03541.png",
+    "tetaaru03553.png",
+    "tetaaru03565.png",
+    "tetaaru03577.png",
+    "tetaaru03589.png",
+    "tetaaru03601.png",
+    "tetaaru03613.png",
+    "tetaaru03625.png",
+    "tetaaru03637.png",
+    "tetaaru03649.png",
+    "tetaaru03661.png",
+    "tetaaru03673.png",
+    "tetaaru03685.png",
+    "tetaaru03697.png",
+    "tetaaru03709.png",
+    "tetaaru03721.png",
+    "tetaaru03733.png",
+    "tetaaru03745.png",
+    "tetaaru03757.png",
+    "tetaaru03769.png",
+    "tetaaru03781.png",
+    "tetaaru03793.png",
+    "tetaaru03805.png",
+    "tetaaru03817.png",
+    "tetaaru03829.png",
+    "tetaaru03841.png",
+    "tetaaru03853.png",
+    "tetaaru03865.png",
+    "tetaaru03877.png",
+    "tetaaru03889.png",
+    "tetaaru03901.png",
+    "tetaaru03913.png",
+    "tetaaru03925.png",
+    "tetaaru03937.png",
+    "tetaaru03949.png",
+    "tetaaru03961.png",
+    "tetaaru03973.png",
+    "tetaaru03985.png",
+    "tetaaru03997.png",
+    "tetaaru04009.png",
+    "tetaaru04021.png",
+    "tetaaru04033.png",
+    "tetaaru04045.png",
+    "tetaaru04057.png",
+    "tetaaru04069.png",
+    "tetaaru04081.png",
+    "tetaaru04093.png",
+    "tetaaru04105.png",
+    "tetaaru04117.png",
+    "tetaaru04129.png",
+    "tetaaru04141.png",
+    "tetaaru04153.png",
+    "tetaaru04165.png",
+    "tetaaru04177.png",
+    "tetaaru04189.png",
+    "tetaaru04201.png",
+    "tetaaru04213.png",
+    "tetaaru04225.png",
+    "tetaaru04237.png",
+    "tetaaru04249.png",
+    "tetaaru04261.png",
+    "tetaaru04273.png",
+    "tetaaru04285.png",
+    "tetaaru04297.png",
+    "tetaaru04309.png",
+    "tetaaru04321.png",
+    "tetaaru04333.png",
+    "tetaaru04345.png",
+    "tetaaru04357.png",
+    "tetaaru04369.png",
+    "tetaaru04381.png",
+    "tetaaru04393.png",
+    "tetaaru04405.png",
+    "tetaaru04417.png",
+    "tetaaru04429.png",
+    "tetaaru04441.png",
+    "tetaaru04453.png",
+    "tetaaru04465.png",
+    "tetaaru04477.png",
+    "tetaaru04489.png",
+    "tetaaru04501.png",
+    "tetaaru04513.png",
+    "tetaaru04525.png",
+    "tetaaru04537.png",
+    "tetaaru04549.png",
+    "tetaaru04561.png",
+    "tetaaru04573.png",
+    "tetaaru04585.png",
+    "tetaaru04597.png",
+    "tetaaru04609.png",
+    "tetaaru04621.png",
+    "tetaaru04633.png",
+    "tetaaru04645.png",
+    "tetaaru04657.png",
+    "tetaaru04669.png",
+    "tetaaru04681.png",
+    "tetaaru04693.png",
+    "tetaaru04705.png",
+    "tetaaru04717.png",
+    "tetaaru04729.png",
+    "tetaaru04741.png",
+    "tetaaru04753.png",
+    "tetaaru04765.png",
+    "tetaaru04777.png",
+    "tetaaru04789.png",
+    "tetaaru04801.png",
+    "tetaaru04813.png",
+    "tetaaru04825.png",
+    "tetaaru04837.png",
+    "tetaaru04849.png",
+    "tetaaru04861.png",
+    "tetaaru04873.png",
+    "tetaaru04885.png",
+    "tetaaru04897.png",
+    "tetaaru04909.png",
+    "tetaaru04921.png",
+    "tetaaru04933.png",
+    "tetaaru04945.png",
+    "tetaaru04957.png",
+    "tetaaru04969.png",
+    "tetaaru04981.png",
+    "tetaaru04993.png",
+    "tetaaru05005.png",
+    "tetaaru05017.png",
+    "tetaaru05029.png",
+    "tetaaru05041.png",
+    "tetaaru05053.png",
+    "tetaaru05065.png",
+    "tetaaru05077.png",
+    "tetaaru05089.png",
+    "tetaaru05101.png",
+    "tetaaru05113.png",
+    "tetaaru05125.png",
+    "tetaaru05137.png",
+    "tetaaru05149.png",
+    "tetaaru05161.png",
+    "tetaaru05173.png",
+    "tetaaru05185.png",
+    "tetaaru05197.png",
+    "tetaaru05209.png",
+    "tetaaru05221.png",
+    "tetaaru05233.png",
+    "tetaaru05245.png",
+    "tetaaru05257.png",
+    "tetaaru05269.png",
+    "tetaaru05281.png",
+    "tetaaru05293.png",
+    "tetaaru05305.png",
+    "tetaaru05317.png",
+    "tetaaru05329.png",
+    "tetaaru05341.png",
+    "tetaaru05353.png",
+    "tetaaru05365.png",
+    "tetaaru05377.png",
+    "tetaaru05389.png",
+    "tetaaru05401.png",
+    "tetaaru05413.png",
+    "tetaaru05425.png",
+    "tetaaru05437.png",
+    "tetaaru05449.png",
+    "tetaaru05461.png",
+    "tetaaru05473.png",
+    "tetaaru05485.png",
+    "tetaaru05497.png",
+    "tetaaru05509.png",
+    "tetaaru05521.png",
+    "tetaaru05533.png",
+    "tetaaru05545.png",
+    "tetaaru05557.png",
+    "tetaaru05569.png",
+    "tetaaru05581.png",
+    "tetaaru05593.png"
+];
+
 window.addEventListener("load",function() {
 
 var Q = window.Q = Quintus()
@@ -68,15 +536,6 @@ Q.Sprite.extend("Stone", {
            // Q.audio.play('pushing_stone.mp3');
            this.p.vx = 60;
          } 
-      }
-    });
-
-    this.on("bump.bottom",function(collision) {
-      var obj = collision.obj;
-      if(obj.isA("Radio")) { 
-           this.p.vy -= 50 * (collision.impact/150);
-      } else {
-        this.p.vx = 0;
       }
     });
   }
@@ -183,13 +642,11 @@ Q.Sprite.extend("Gerev",{
     this.p.ignoreControls = false;
     // this.p.sprite = "gerev_glow";
     // this.p.sheet = "gerev_walk_glow";
-    this.p.scale = 1.2;
   },
 
   stopInteraction: function() {
     this.play("gerev_stand_" + this.p.direction);
     this.p.ignoreControls = true;
-    this.p.scale = 1;
     // this.p.sprite = "gerev"
     // this.p.sheet = "gerev_walk";
     this.p.vx = 0;
@@ -309,34 +766,6 @@ Q.Sprite.extend("Zemer",{
 
 });
 
-Q.Sprite.extend("Radio",{
-
-  // the init constructor is called on creation
-  init: function(p) {
-
-    // You can call the parent's constructor with this._super(..)
-    this._super(p, {
-      sheet: "radio_coloring",  // Setting a sprite sheet sets sprite width and height
-      sprite: "radio",
-      x: 975,           // You can also set additional properties that can
-      y: 571,             // be overridden on object creation
-      scale: 0.35
-    });
-    this.add('2d,animation');
-    
-    this.p.playedColoringAnimation = false;
-
-    this.on("bump.top",function(collision) {
-      var obj = collision.obj;
-      if(obj.isA("Stone") && !this.p.playedColoringAnimation) { 
-           this.p.playedColoringAnimation = true;
-           this.play("radio_coloring");
-      } 
-    });
-  }
-
-});
-
 Q.Sprite.extend("Tv",{
 
   // the init constructor is called on creation
@@ -346,8 +775,8 @@ Q.Sprite.extend("Tv",{
     this._super(p, {
       sheet: "tv",  // Setting a sprite sheet sets sprite width and height
       sprite: "tv",
-      x: 1132,           // You can also set additional properties that can
-      y: 468,             // be overridden on object creation
+      x: 1043,           // You can also set additional properties that can
+      y: 530,             // be overridden on object creation
       z: 100
     });
     this.add('animation, tween');
@@ -360,12 +789,25 @@ Q.Sprite.extend("Tv",{
            this.p.playedMoveAnimation = true;
            this.moveLeft();
            Q.GameStatus.phase = "invitation";
+           Q.tvScreen.p.opacity = 1;
+
+           setInterval(function() {
+              if (Q.GameStatus.phase=="invitation" && Q.tvScreen.p.opacity==1) {
+                Q.tvScreen.p.asset = Q.tvScreen.frames[Q.tvScreen.animIndex % Q.tvScreen.frames.length];
+                Q.tvScreen.animIndex++;
+              }
+            }, 1000 * 10 / 60);
       } 
     });
   },
 
   moveLeft: function() {
-    this.animate({x: -500, y: 468}, 10);
+    this.animate({x: 300, y: 530}, 11);
+  },
+
+  step: function(dt) {
+    Q.tvScreen.p.x = this.p.x+17;
+    Q.tvScreen.p.y = this.p.y-10;
   }
 
 });
@@ -379,7 +821,7 @@ Q.Sprite.extend("Invitation",{
     this._super(p, {
       sheet: "invitation",  // Setting a sprite sheet sets sprite width and height
       sensor: true,
-      x: 1700,           // You can also set additional properties that can
+      x: 1420,           // You can also set additional properties that can
       y: 340,             // be overridden on object creation
       z: 100
     });
@@ -397,7 +839,7 @@ Q.Sprite.extend("Invitation",{
   },
 
   moveLeft: function() {
-    this.animate({x: 512, y: 340}, 13);
+    this.animate({x: 680, y: 340}, 12);
   }
 
 });
@@ -493,14 +935,6 @@ Q.scene("level1",function(stage) {
 
   Q.instructions = stage.insert(new Q.Sprite({x: 512, y: 340, asset: "instructions1.png", sensor: true}));
 
-  /*
-  stage.insert(new Q.UI.Text({x:750, y: 13, opacity: 1, label: "הזזת ארנב" }));
-  stage.insert(new Q.Sprite({x: 850, y: 28, scale: 0.5, opacity: 1, asset: "arrows.png", sensor: true}));
-
-  stage.insert(new Q.UI.Text({x:740, y: 51, opacity: 1, label: "החלפת ארנב" }));
-  stage.insert(new Q.Sprite({x: 850, y: 66, scale: 0.5, asset: "spacebar.png", sensor: true}));
-  */
-
   // Create the player and add them to the stage
   var gerev = Q.gerev = stage.insert(new Q.Gerev());
   var zemer = Q.zemer = stage.insert(new Q.Zemer());
@@ -510,12 +944,16 @@ Q.scene("level1",function(stage) {
   zemer.stopInteraction();
 
   var tv = Q.tv = stage.insert(new Q.Tv());
+  Q.tvScreen = stage.insert(new Q.Sprite({x: 200, y: 200, asset: "tetaaru00025.png", sensor: true, opacity: 0}));
+  Q.tvScreen.frames = SONG_FRAMES;
+  Q.tvScreen.animIndex = 0;
+
   var invitation = Q.invitation = stage.insert(new Q.Invitation());
 
 });
 
 Q.load(
-  "background_music.mp3, gerev_jump.mp3, gerev_walk.mp3, powerup.mp3, press_switch.mp3, pushing_stone.mp3, switch_rabbit.mp3, zemer_jump.mp3, zemer_walk.mp3, tiles.png, instructions1.png, spacebar.png, arrows.png, gerev_walk.png, gerev_walk_glow.png, gerev_walk.json, gerev_walk_glow.json, zemer_walk.png, zemer_walk_glow.png, zemer_walk.json, zemer_walk_glow.json, carrot.png, stone.png, cabbage.png, on_switch.png, off_switch.png, background-wall.png, level.json, mmc_map_clouds.png, tv.png, invitation.png", 
+  SONG_FRAMES.toString() + ", background_music.mp3, gerev_jump.mp3, gerev_walk.mp3, powerup.mp3, press_switch.mp3, pushing_stone.mp3, switch_rabbit.mp3, zemer_jump.mp3, zemer_walk.mp3, tiles.png, instructions1.png, gerev_walk.png, gerev_walk_glow.png, gerev_walk.json, gerev_walk_glow.json, zemer_walk.png, zemer_walk_glow.png, zemer_walk.json, zemer_walk_glow.json, carrot.png, stone.png, cabbage.png, on_switch.png, off_switch.png, background-wall.png, level.json, tv.png, invitation.png",
   function() {
     Q.compileSheets("zemer_walk_glow.png","zemer_walk_glow.json");
     Q.compileSheets("gerev_walk_glow.png","gerev_walk_glow.json");
@@ -526,9 +964,7 @@ Q.load(
     // Sprites sheets can be created manually
     Q.sheet("tiles","tiles.png", { tilew: 32, tileh: 32 });
     Q.sheet("invitation","invitation.png", { tilew: 521, tileh: 645 });
-    // Q.sheet("radio","radio.png", { tilew: 252, tileh: 217 });
-    Q.sheet("tv","tv.png", { tilew: 408, tileh: 281 });
-    Q.sheet("clouds","mmc_map_clouds.png", { tilew: 800, tileh: 250 });
+    Q.sheet("tv","tv.png", { tilew: 230, tileh: 158 });
     Q.sheet("stone","stone.png", { tilew: 32, tileh: 32 });
     Q.sheet("on_switch","on_switch.png", { tilew: 32, tileh: 32 });
     Q.sheet("off_switch","off_switch.png", { tilew: 32, tileh: 32 });
@@ -581,10 +1017,6 @@ Q.load(
       gerev_stand_left: { frames: [5], rate: 1/15, flip: false, loop: false},
       gerev_stand_right: { frames: [5], rate: 1/15, flip: "x", loop: false}
     });
-
-    // Q.animations("radio", {
-    //   radio_coloring: { frames: [0,0,1,1,2,2,3,3,4,4,0,0,1,1,2,2,3,3,4,4], rate: 1/8, flip: false, loop: false}
-    // });
 
     // Finally, call stageScene to run the game
     Q.stageScene("level1");
